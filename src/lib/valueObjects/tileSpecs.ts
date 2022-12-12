e1bd155118c257744a72dc7c5a3a60ce19afd6fc
Torsten Sprenger <mail@spren9er.de>
2022-12-12 21:36:03 +0100

    Remove d3-scale dependency

--- src/lib/valueObjects/tileSpecs.ts
+++ src/lib/valueObjects/tileSpecs.ts
@@ -1,5 +1,3 @@
-import { scaleLinear } from 'd3-scale';
-
 export class TileSpecs {
   constructor(
     public width: number,
@@ -11,40 +9,58 @@ export class TileSpecs {
   ) {}
 
   public x(x: number) {
-    return scaleLinear([0, this.width], [this.relX, this.relX + this.width])(x);
+    return this.scaleLinear(
+      [0, this.width],
+      [this.relX, this.relX + this.width],
+    )(x);
   }
 
   public xRev(x: number) {
-    return scaleLinear([0, this.width], [this.relX + this.width, this.relX])(x);
+    return this.scaleLinear(
+      [0, this.width],
+      [this.relX + this.width, this.relX],
+    )(x);
   }
 
   public y(y: number) {
-    return scaleLinear(
+    return this.scaleLinear(
       [0, this.height],
       [this.relY, this.relY + this.height],
     )(y);
   }
 
   public yRev(y: number) {
-    return scaleLinear(
+    return this.scaleLinear(
       [0, this.height],
       [this.relY + this.height, this.relY],
     )(y);
   }
 
   public pX(x: number) {
-    return scaleLinear([0, 1], [this.relX, this.relX + this.width])(x);
+    return this.scaleLinear([0, 1], [this.relX, this.relX + this.width])(x);
   }
 
   public pXRev(x: number) {
-    return scaleLinear([0, 1], [this.relX + this.width, this.relX])(x);
+    return this.scaleLinear([0, 1], [this.relX + this.width, this.relX])(x);
   }
 
   public pY(y: number) {
-    return scaleLinear([0, 1], [this.relY, this.relY + this.height])(y);
+    return this.scaleLinear([0, 1], [this.relY, this.relY + this.height])(y);
   }
 
   public pYRev(y: number) {
-    return scaleLinear([0, 1], [this.relY + this.height, this.relY])(y);
+    return this.scaleLinear([0, 1], [this.relY + this.height, this.relY])(y);
+  }
+
+  private scaleLinear(
+    domain: number[],
+    range: number[],
+  ): (x: number) => number {
+    const [x1, x2] = domain;
+    const [y1, y2] = range;
+
+    if (x1 === x2) return () => (y2 - y1) / 2;
+
+    return (x: number) => (y1 * (x2 - x) + y2 * (x - x1)) / (x2 - x1);
   }
 }
