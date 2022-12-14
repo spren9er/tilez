b6b178226c88155cbeeaf0270a5aafaafbaeeb63
Torsten Sprenger <mail@spren9er.de>
2022-12-14 21:18:46 +0100

    Bump sveltekit to v1.0 and add test for tile context

--- tests/entities/tileContext.test.ts
+++ tests/entities/tileContext.test.ts
@@ -1,7 +1,11 @@
-import { beforeEach, expect, describe, it, vi } from 'vitest';
+import { expect, describe, it, vi } from 'vitest';
+
+import { get } from 'svelte/store';
+
+import { getSpecsContext, setNodeContext } from '$lib/entities/tileContext';
 
 // mock svelte context
-let CONTEXT = new Map();
+const CONTEXT = new Map();
 vi.mock('svelte', () => ({
   getContext(key: string) {
     return CONTEXT.get(key);
@@ -15,8 +19,13 @@ vi.mock('svelte', () => ({
 }));
 
 describe('TileContext', () => {
-  beforeEach(() => {
-    CONTEXT = new Map();
+  it('creates node/specs contexts and retrieve specs of context', () => {
+    const width = 200;
+    const height = 100;
+    setNodeContext({ width, height });
+
+    const specs = getSpecsContext();
+    expect(get(specs).width).toEqual(width);
+    expect(get(specs).height).toEqual(height);
   });
-  it('creates node context', () => {});
 });
