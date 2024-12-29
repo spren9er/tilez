4823a62197ec1fac0a066ae6c9d7ee762b137104
Torsten Sprenger <mail@spren9er.de>
2024-12-29 17:59:53 +0100

    Update to Vite 6 / Remove testing-library (#3)
    * Update to vite 6
    
    * Remove components from tests
    
    * Update package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), svelteTesting()],
+  plugins: [
+    // @ts-expect-error
+    sveltekit({
+      compilerOptions: {
+        runes: true,
+        css: 'injected',
+      },
+    }),
+  ],
   test: {
+    browser: {
+      provider: 'webdriverio',
+      enabled: true,
+      name: 'chrome',
+      headless: true,
+      isolate: true,
+      fileParallelism: true,
+      screenshotFailures: false,
+    },
     globals: true,
     include: ['tests/**/*.test.ts'],
-    environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     coverage: {
       include: ['src/lib/**/*.{ts,svelte}'],
-      exclude: ['src/lib/index.ts', 'src/lib/types/**/*'],
+      exclude: [
+        'src/lib/index.ts',
+        'src/lib/types/**/*',
+        'src/lib/components/**/*',
+      ],
+      provider: 'istanbul',
     },
   },
   resolve: {
